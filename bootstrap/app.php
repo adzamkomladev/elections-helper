c49fabb2581370b1d724daf427242376cc51877c
Komla Adzam <adzamkomla.dev@gmail.com>
2024-07-24 20:16:24 +0000

    feat: setup libraries

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->appendToGroup('web', [\App\Http\Middleware\HandleHybridRequests::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
